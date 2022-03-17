8ac86d7fb919029d79d93ef6be01d87d4f9853a1
leonardosprado <leonardoprado19@gmail.com>
2022-03-17 18:58:40 -0300

    Alterações da Aula do dia 17/03

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,6 +3,29 @@ var user = document.getElementById('user');
 
 console.log("HELLO WORLD!");
 window.entrar = function(){
-    event.preventDefault(); 
-    console.log("Clique");
+    event.preventDefault();
+    var usuario = document.getElementById('user').value;
+    var searchUser = buscarUser(usuario); 
+    var senha = document.getElementById('senha').value;
+
+    console.log(searchUser);
+}
+
+function searchUser(usuario){ /// 
+    var aux = false; //Variaveis boleanas 
+    var user = null;
+    var i=0; 
+    userApi.forEach(element => {
+        i++; /// i = i + 1;
+        if(element.username === usuario){
+            aux = true;
+            user = element;
+            return;
+        }
+    });
+    return{aux,user,i};
+}
+
+function validarSenha(usuario){
+
 }
